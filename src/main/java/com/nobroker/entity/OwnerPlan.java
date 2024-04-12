e2a1c078e75c33d4be61d5a4d264ffb7cf0926dc
Kishankumar57 <ishankishan57@gmail.com>
2024-04-12 17:40:45 +0530

    8th jan 2024

--- src/main/java/com/nobroker/entity/OwnerPlan.java
+++ src/main/java/com/nobroker/entity/OwnerPlan.java
@@ -19,20 +19,28 @@ public class OwnerPlan {
 
     @Column(name = "plan_name", unique = true)
     private String PlanName;
+
     @Column(name = "price")
     private double price;
+
     @Column(name = "plan_validity")
     private int planValidity;
+
     @Column(name = "relationship_manager")
     private boolean RelationshipManager;
+
     @Column(name = "rental_agreement")
     private boolean RentalAgreement;
+
     @Column(name = "property_promotion")
     private boolean propertyPromotion;
+
     @Column(name = "guaranteed_tenants")
     private boolean guaranteedTenants;
+
     @Column(name = "showing_property")
     private boolean showingProperty;
+
     @Column(name = "facebook_marketing_of_property")
     private boolean facebookMarketingOfProperty;
 }
